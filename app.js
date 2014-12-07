bf0a803fd71bd71e9ee9a57fef9b08580dec0bbf
Aaron <ans.ballard@gmail.com>
2014-12-06 23:40:59 -0600

    Big download button

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var mongoose = require('mongoose/');
 //var Modlist = require('./models/modlist');
 //var Blog = require('./models/blog');
 
-var configDB = require('./config/db.js').localurl;
+var configDB = require('./config/db.js').url;
 mongoose.connect(configDB);
 
 var Schema = mongoose.Schema;
