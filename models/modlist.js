a4985cc4884fa0e736ff9ef93ce8b4edbe059391
Aaron <ans.ballard@gmail.com>
2014-12-14 11:31:27 -0600

    reverted terrible file array changes

--- models/modlist.js
+++ models/modlist.js
@@ -31,39 +31,4 @@ modlistSchema.methods.validPassword = function(_password) {
 	return bcrypt.compareSync(_password, this.password);
 };
 
-modlistSchema.methods.convertFilesToArrays = function() {
-
-	this.plugins = null;
-	this.modlist = null;
-	this.ini = null;
-	this.prefsini = null;
-
-	var temp = [];
-	temp = this.list.split('\",\"');
-	temp[0] = this.plugins[0].substring(2,this.plugins[0].length);
-	
-
-	for(var i = 0; i < temp.length; i++) {
-		this.plugins[i].name = temp[i];
-	}
-	this.plugins = this.list.split('\",\"');
-	this.plugins[0] = this.plugins[0].substring(2,this.plugins[0].length);
-	this.plugins[this.plugins.length-1] = this.plugins[this.plugins.length-1].substring(0,this.plugins[this.plugins.length-1].length-2);
-
-	this.modlist = this.modlisttxt.split('\",\"');
-	this.modlist[0] = this.modlist[0].substring(2,this.modlist[0].length);
-	this.modlist[this.modlist.length-1] = this.modlist[this.modlist.length-1].substring(0,this.modlist[this.modlist.length-1].length-2);
-
-	this.ini = this.skyrimini.split('\",\"');
-	this.ini[0] = this.ini[0].substring(2,this.ini[0].length);
-	this.ini[this.ini.length-1] = this.ini[this.ini.length-1].substring(0,this.ini[this.ini.length-1].length-2);
-
-	this.prefsini = this.skyrimprefsini.split('\",\"');
-	this.prefsini[0] = this.prefsini[0].substring(2,this.prefsini[0].length);
-	this.prefsini[this.prefsini.length-1] = this.prefsini[this.prefsini.length-1].substring(0,this.prefsini[this.prefsini.length-1].length-2);
-
-	this.save();
-	return true;
-};
-
 module.exports = mongoose.model('Modlist', modlistSchema);
--- routes.js
+++ routes.js
@@ -1,13 +1,5 @@
 module.exports = function(app, passport, scriptVersion) {
 
-	/*app.get('/testOverwrite/:username', function(req, res) {
-		Modlist.findOne({username: req.param("username")}, function(err, _lists) {
-			_lists.convertFilesToArrays();
-			res.writeHead(200);
-			res.end();
-		});
-	});*/
-
 	app.get('/', function(req, res) {
 		Blog.findOne({'newest': true}, function(err, _blog) {
 			Modlist.find({}, function(err, _mods) {
